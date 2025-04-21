ece97826281de2fd6a607746c0017cc7d352f0a7
anspear-bi <a.starikova3@g.nsu.ru>
2025-04-21 09:24:51 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,6 @@
-with open('input.txt', 'r') as f_in:
-    lines = f_in.readlines()
+f = open('input.txt', 'r')
 
-with open('output.txt', 'w') as f_out:
-    for line in lines:
-        if len(line.rstrip('\n')) > 20:
-            f_out.write(line)
+for i in f.readlines():
+    if len(i) >= 20:
+        print(i)
+f.close()
