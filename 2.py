ef1ff486e96c940177b0c3b89ba908cd06ba6eb0
anspear-bi <a.starikova3@g.nsu.ru>
2025-04-21 09:18:24 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
-with open('input.txt', 'r') as f_in:
-    lines = f_in.readlines()
+f = open('input.txt', 'r')
 
-with open('output.txt', 'w') as f_out:
-    for line in lines:
-        if line.startswith('A'):
-            f_out.write(line)
+for i in f.readlines():
+    if i[0].lower() == 'a':
+        print(i)
+f.close()
+
