be34704f5fcba011469accdd6fdcec25bbf37c39
anspear-bi <a.starikova3@g.nsu.ru>
2025-04-21 09:26:49 +0700

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,7 +1,8 @@
-with open('input.txt', 'r') as f_in:
-    lines = f_in.readlines()
+f = open('input.txt', 'r')
+fn = open('output.txt', 'w')
 
-with open('input.txt', 'w') as f_out:
-    for line in lines:
-        if line.strip() != '100':
-            f_out.write(line)
+for i in f:
+    if '100' == i:
+        pass
+    else:
+        fn.write(f'{i}')
