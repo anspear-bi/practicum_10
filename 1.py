9ec97cd1d97c4d13c22382b838f23ee2ec1d0720
anspear-bi <a.starikova3@g.nsu.ru>
2025-04-21 09:17:49 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,4 @@
-with open('input.txt', 'r') as f_in:
-    data = f_in.read()
-
-with open('output.txt', 'w') as f_out:
-    f_out.write(data.upper())
+f = str(*open('input.txt', 'r'))
+fn = open('output.txt', 'w')
+fn.write(f.upper())
     
